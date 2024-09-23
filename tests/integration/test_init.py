e6ba554da77894809ede5485f41a743fc5d8b123
Kate Case <kcase@redhat.com>
2024-09-23 10:40:37 -0400

    cleanup test

--- tests/integration/test_init.py
+++ tests/integration/test_init.py
@@ -130,14 +130,9 @@ def test_run_init_basic(cli: CliRunCallable, tmp_path: Path) -> None:
     # this is required to handle random line breaks in CI, especially with macos runners
     mod_stderr = "".join([line.strip() for line in result.stderr.splitlines()])
     assert (
-        re.search(
-            rf"Error:\s*The\s*directory\s*{final_dest}/testorg/testcol\s*is\s*not\s*empty.",
-            mod_stderr,
-        )
-        is not None
+        f"Not overwriting files in {final_dest}/testorg/testcol as --force was not specified."
+        in mod_stderr
     )
-    assert "You can use --force to re-initialize this directory." in result.stderr
-    assert "However it will delete ALL existing contents in it." in result.stderr
 
     # override existing collection with force=true
     result = cli(f"{CREATOR_BIN} init testorg.testcol --init-path {tmp_path} --force")
